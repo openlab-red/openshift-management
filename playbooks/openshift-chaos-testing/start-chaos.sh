eac03677553289890f558bc321799ddd900fb37d
Sudhir Pandey <sudpande@redhat.com>
2019-04-10 17:30:27 +0200

    wrapper script to launch chaos testing and sleeps from 30min to 60min

--- playbooks/openshift-chaos-testing/start-chaos.sh
+++ playbooks/openshift-chaos-testing/start-chaos.sh
@@ -1,17 +1,23 @@
-#! /bin/bash
+#!/bin/bash
 
 LIMIT_GROUPS=( nodes masters nodes:!masters )
-TASK=( "crash_node" "crash_etcd_leader" )
+TASK=( crash_node crash_etcd_leader )
 
-while true;
+while true
 do
 tsize=${#TASK[@]}
 tindex=$(($RANDOM % $tsize))
 
-lsize=${#TASK[@]}
-lindex=$(($RANDOM % $tsize))
+lsize=${#LIMIT_GROUPS[@]}
+lindex=$(($RANDOM % $lsize))
 
+sleep_min=30
+sleep_range=30
+sleep_time=$[ ( $RANDOM % $sleep_range )  + $sleep_min ]
 
 echo "executing: ansible-playbook -i inventory/hosts testing.yaml -l '${LIMIT_GROUPS[$lindex]}' -t ${TASK[$tindex]}"
-sleep $[ ( $RANDOM % 10 )  + 1 ]s
+
+echo "sleeping $sleep_time minutes"
+
+sleep "$sleep_time"m
 done
