33a9a1206356bb4a8f78e6b1eb38496a7aa4bb34
Sudhir Pandey <sudpande@redhat.com>
2019-04-10 18:06:19 +0200

    make inventory file parameterized

--- playbooks/openshift-chaos-testing/start-chaos.sh
+++ playbooks/openshift-chaos-testing/start-chaos.sh
@@ -2,6 +2,7 @@
 
 LIMIT_GROUPS=( nodes masters nodes:!masters )
 TASK=( crash_node crash_etcd_leader )
+INVENTORY=$1
 
 while true
 do
@@ -15,7 +16,7 @@ sleep_min=30
 sleep_range=30
 sleep_time=$[ ( $RANDOM % $sleep_range )  + $sleep_min ]
 
-echo "executing: ansible-playbook -i inventory/hosts config.yaml -l '${LIMIT_GROUPS[$lindex]}' -t ${TASK[$tindex]}"
+echo "executing: ansible-playbook -i ${INVENTORY} config.yaml -l '${LIMIT_GROUPS[$lindex]}' -t ${TASK[$tindex]}"
 
 echo "sleeping $sleep_time minutes"
 
