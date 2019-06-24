abfeacf4bd8757a4cea1ec249f6a2228dd201c96
rlourenc <rlourenc@redhat.com>
2019-06-24 13:04:24 +0200

    WIP: docker_gc fix

--- roles/docker_gc/files/docker-gc.sh
+++ roles/docker_gc/files/docker-gc.sh
@@ -7,12 +7,34 @@ MKTEMP=$(dirname $(realpath $0))
 function containers {
     case $1 in
         --check)
-            docker ps -qa --filter status=exited --filter status=dead
+            exited=$(docker ps -qa --filter status=exited --filter status=dead)
+            inspect $exited
             ;;
         --run)
-            docker rm -v $(docker ps -qa --filter status=exited --filter status=dead)
+            exited=$(docker ps -qa --filter status=exited --filter status=dead)
+            to_remove=$(inspect $exited)
+            docker rm -v -f $to_remove 2>/dev/null
             ;;
     esac
+}
+
+function inspect {
+  for container in ${1}
+    do
+      label=$(docker inspect $container -f '{{index .Config.Labels "io.kubernetes.pod.name"}}')
+      return_running ${label}
+      getval=$(return_running ${label})
+    done
+  echo ${getval}
+}
+
+function return_running {
+    running_containers_by_label=$(docker ps -q --filter status=running --filter "label=io.kubernetes.pod.name=${1}")
+    for container in $running_containers_by_label
+    do
+        running_list+=" $(docker inspect $container -f '{{.Id}}') "
+    done
+    echo $running_list
 }
 
 function images {
@@ -48,4 +70,3 @@ case ${TYPE} in
         exit 1
         ;;
 esac
-
